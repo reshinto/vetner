91a2cde95e195cc1ec2acf08a2425f7cfec4d4c4
reshinto <terencekong2002@gmail.com>
2019-11-11 18:46:37 +0800

    Modified routes for auto routing after registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :user_profiles
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :vets
-  devise_for :users
+  devise_for :vets, controllers: {registrations: "registrations"}
+  devise_for :users, controllers: {registrations: "registrations"}
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :user_profiles
