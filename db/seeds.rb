fceee5218ee127cc854599cb0978fc4561946df8
reshinto <terencekong2002@gmail.com>
2019-11-11 15:19:40 +0800

    Add seeds for admins, users and vets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: "admin@email.com", password: "password", admin: true)
+User.create(email: "terence@email.com", password: "password", admin: true)
+User.create(email: "hongjin@email.com", password: "password", admin: true)
+User.create(email: "simyen@email.com", password: "password", admin: true)
+User.create(email: "user1@email.com", password: "password")
+User.create(email: "user2@email.com", password: "password")
+User.create(email: "user3@email.com", password: "password")
+
+Vet.create(email: "vet1@email.com", password: "password")
+Vet.create(email: "vet2@email.com", password: "password")
+Vet.create(email: "vet3@email.com", password: "password")
