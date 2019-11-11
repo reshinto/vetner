9894d699b7fe7eefea8951bfdcce7e203c7fa349
lhjd <hongjinlau@gmail.com>
2019-11-11 18:05:53 +0800

    Redo user_profile

---
+++ db/migrate/20191111095758_create_user_profiles.rb
@@ -0,0 +1,15 @@
+class CreateUserProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_profiles do |t|
+      t.string :first_name
+      t.string :last_name
+      t.text :address
+      t.string :phone
+      t.string :username
+      t.text :image
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/controllers/user_profiles_controller_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class UserProfilesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user_profile = user_profiles(:one)
+  end
+
+  test "should get index" do
+    get user_profiles_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_user_profile_url
+    assert_response :success
+  end
+
+  test "should create user_profile" do
+    assert_difference('UserProfile.count') do
+      post user_profiles_url, params: { user_profile: { address: @user_profile.address, first_name: @user_profile.first_name, image: @user_profile.image, last_name: @user_profile.last_name, phone: @user_profile.phone, user_id: @user_profile.user_id, username: @user_profile.username } }
+    end
+
+    assert_redirected_to user_profile_url(UserProfile.last)
+  end
+
+  test "should show user_profile" do
+    get user_profile_url(@user_profile)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_user_profile_url(@user_profile)
+    assert_response :success
+  end
+
+  test "should update user_profile" do
+    patch user_profile_url(@user_profile), params: { user_profile: { address: @user_profile.address, first_name: @user_profile.first_name, image: @user_profile.image, last_name: @user_profile.last_name, phone: @user_profile.phone, user_id: @user_profile.user_id, username: @user_profile.username } }
+    assert_redirected_to user_profile_url(@user_profile)
+  end
+
+  test "should destroy user_profile" do
+    assert_difference('UserProfile.count', -1) do
+      delete user_profile_url(@user_profile)
+    end
+
+    assert_redirected_to user_profiles_url
+  end
+end
