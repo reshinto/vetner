11eb4b0da36b96e7ff5467da327c63a516e0e058
reshinto <terencekong2002@gmail.com>
2019-11-14 11:50:21 +0800

    Add unit to user profile table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_034732) do
+ActiveRecord::Schema.define(version: 2019_11_14_034934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_034732) do
     t.datetime "updated_at", null: false
     t.string "country"
     t.string "postalcode"
+    t.string "unit"
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
