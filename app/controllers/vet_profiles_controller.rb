d5e856df2c75d2bf5da604c9150c396989b2dd0d
reshinto <terencekong2002@gmail.com>
2019-11-17 04:57:08 +0800

    Use leaflet in vet profile show view

--- app/controllers/vet_profiles_controller.rb
+++ app/controllers/vet_profiles_controller.rb
@@ -4,16 +4,19 @@ class VetProfilesController < ApplicationController
 
   before_action :authenticate_vet!, only: [:edit, :update, :destroy]
 
+  @@base_url = "https://developers.onemap.sg"
   @@baseOnemapUrl = "https://www.onemap.sg/amm/amm.html?mapStyle=Default&zoomLevel=15&width=1000&height=450"
+  @@token = ENV["ONEMAPTOKEN"]
+  @@distData = Hash.new
 
   # GET /vet_profiles
   # GET /vet_profiles.json
   def index
-    # if a params[:search] is received
+    require "open-uri"
+    # # if a params[:search] is received
     if params[:search]
       # search for vet profiles using the model method `search_vet_profiles`
       # passing in the search parameters, `params[:search]`
-      # this method is implemented using the pg_search gem 
       @vet_profiles = VetProfile.search_vet_profiles(params[:search])
     else
       # otherwise, retrive all vet profiles
@@ -22,13 +25,24 @@ class VetProfilesController < ApplicationController
 
     # prepare the query strings required for OneMap API
     vetPositions = ""
-    @vet_profiles.each do |vp|
-      vetPositions += "&marker=latLng:#{vp.vetLat},#{vp.vetLong}!icon:fa-plus!colour:lightblue"
-    end
-
     if current_user
       user_profile = UserProfile.find(current_user.id)
       userPosition = "&marker=latLng:#{user_profile.userLat},#{user_profile.userLong}!icon:fa-user!colour:red"
+      @vet_profiles.each do |vp|
+        distanceparams = "/privateapi/routingsvc/route?start=#{user_profile.userLat},#{user_profile.userLong}&end=#{vp.vetLat},#{vp.vetLong}&routeType=walk&token=#{@@token}"
+        if @@distData["#{distanceparams}"] == nil
+          result = JSON.load(open("#{@@base_url}#{distanceparams}"))
+          distance = result["route_summary"]["total_distance"]
+          @@distData["#{distanceparams}"] = distance
+        else
+          distance = @@distData["#{distanceparams}"]
+        end
+        if distance <= 5000
+          vetPositions += "&marker=latLng:#{vp.vetLat},#{vp.vetLong}#{vp.popupdetails}!icon:fa-plus!colour:lightblue"
+        else
+          vetPositions += ""
+        end
+      end
       @address = "#{@@baseOnemapUrl}#{userPosition}#{vetPositions}"
     else
       @address = "#{@@baseOnemapUrl}#{vetPositions}"
@@ -38,18 +52,25 @@ class VetProfilesController < ApplicationController
   # GET /vet_profiles/1
   # GET /vet_profiles/1.json
   def show
+    require "open-uri"
     @vet_profile = VetProfile.find(params[:id])
-    vetPosition = "&marker=latLng:#{@vet_profile.vetLat},#{@vet_profile.vetLong}!icon:fa-plus!colour:lightblue"
+    @vetLat = @vet_profile.vetLat
+    @vetLong = @vet_profile.vetLong
     if current_user
       user_profile = UserProfile.find(current_user.id)
-      # address = "#{user_profile.address} #{user_profile.country}"
-      # userResults = Geocoder.search(address).first.coordinates
-      # userLat = userResults[0]
-      # userLong = userResults[1]
-      userPosition = "&marker=latLng:#{user_profile.userLat},#{user_profile.userLong}!icon:fa-user!colour:red"
-      @address = "#{@@baseOnemapUrl}#{userPosition}#{vetPosition}"
+      @userLat = user_profile.userLat
+      @userLong = user_profile.userLong
+      distanceparams = "/privateapi/routingsvc/route?start=#{@userLat},#{@userLong}&end=#{@vetLat},#{@vetLong}&routeType=walk&token=#{@@token}"
+      if @@distData["#{distanceparams}"] == nil
+        result = JSON.load(open("#{@@base_url}#{distanceparams}"))
+        @distance = result["route_summary"]["total_distance"]
+        @@distData["#{distanceparams}"] = @distance
+      else
+        @distance = @@distData["#{distanceparams}"]
+      end
     else
-      @address = "#{@@baseOnemapUrl}#{vetPosition}"
+      @userLat = @vetLat
+      @userLong = @vetLong
     end
   end
 
@@ -88,7 +109,7 @@ class VetProfilesController < ApplicationController
         format.html { redirect_to @vet_profile, notice: 'Vet profile was successfully updated.' }
         format.json { render :show, status: :ok, location: @vet_profile }
 
-        result = JSON.load(open("https://developers.onemap.sg/commonapi/search?searchVal=#{@vet_profile.postalcode}&returnGeom=Y&getAddrDetails=Y"))
+        result = JSON.load(open("#{@base_url}/commonapi/search?searchVal=#{@vet_profile.postalcode}&returnGeom=Y&getAddrDetails=Y"))
         vetLat = result["results"][0]["LATITUDE"]
         vetLong = result["results"][0]["LONGITUDE"]
         address = result["results"][0]["ADDRESS"]
