cf17e002dfc675fe443d481be1409d04906b916b
lhjd <hongjinlau@gmail.com>
2019-11-16 15:18:25 +0800

    Rename search_by_clinic_name

--- app/controllers/vet_profiles_controller.rb
+++ app/controllers/vet_profiles_controller.rb
@@ -11,8 +11,8 @@ class VetProfilesController < ApplicationController
   def index
     # if a params[:search] is received
     if params[:search]
-      # call the model method search_by_clinic_name, defined in VetProfile model
-      @search_results_vet_profiles = VetProfile.search_by_clinic_name(params[:search])
+      # call the model method search_vet_profiles, defined in VetProfile model
+      @search_results_vet_profiles = VetProfile.search_vet_profiles(params[:search])
       # send the search results to the partial view _search-results.js.erb
       # located at views/user_profiles
       respond_to do |format|
